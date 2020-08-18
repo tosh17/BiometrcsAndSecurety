e5a44216bc7eadf3aa2cf00f538840ec29567e91
tosh17 <tosh17@list.ru>
2020-08-18 18:35:58 +0300

    remove biomentic2

--- app/src/main/java/ru/thstdio/biometrcsandsecurety/ui/menu/presenter/MenuPresenter.kt
+++ app/src/main/java/ru/thstdio/biometrcsandsecurety/ui/menu/presenter/MenuPresenter.kt
@@ -4,20 +4,9 @@ import androidx.lifecycle.ViewModel
 import ru.thstdio.biometrcsandsecurety.navigation.AppRouter
 import ru.thstdio.biometrcsandsecurety.navigation.AppScreen
 
-class MenuPresenter(private val router:AppRouter) : ViewModel() {
-    fun clickRootBtn() {
-        router.open(AppScreen.CheckRootScreen)
-    }
-
-    fun clickBiometricBtn() {
-        router.open(AppScreen.BiomentricScreen)
-    }
-
-     fun clickGoogleTinkBtn() {
-        router.open(AppScreen.GoogleTinkScreen)
-    }
-
-    fun clickSharedPreferencesBtn() {
-        router.open(AppScreen.SharedPreferencesScreen)
-    }
+class MenuPresenter(private val router: AppRouter) : ViewModel() {
+    fun clickRootBtn() = router.open(AppScreen.CheckRootScreen)
+    fun clickBiometricBtn() = router.open(AppScreen.BiomentricScreen)
+    fun clickGoogleTinkBtn() = router.open(AppScreen.GoogleTinkScreen)
+    fun clickSharedPreferencesBtn() = router.open(AppScreen.SharedPreferencesScreen)
 }
